112630eca427ba90d651a1b18223c9f8b1aab9fd
BrunoCiprianoDev <cipriano990@gmail.com>
2024-02-12 10:17:03 -0300

    feat(setup):Initialized base Node-API

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/main/*',
+    '<rootDir>/src/domain/util/errors/*',
+  ],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+    '@main/(.*)': '<rootDir>/src/main/$1',
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+  },
+};
---
+++ test/jest-starter.ts
@@ -0,0 +1,8 @@
+import loggerTests from "./config/logger-tests";
+
+
+beforeAll(async () => {
+    loggerTests.info('Test..');
+});
+
+afterAll(async () => { });
