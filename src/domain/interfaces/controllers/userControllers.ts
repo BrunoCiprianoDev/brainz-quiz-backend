e53e1879547ea4cd487dcf46b06e739cd347ad68
BrunoCiprianoDev <cipriano990@gmail.com>
2024-02-16 17:41:33 -0300

    feat: create all controller unit tests

--- src/domain/interfaces/controllers/userControllers.ts
+++ src/domain/interfaces/controllers/userControllers.ts
@@ -4,6 +4,8 @@ import { IUserUseCases } from '@src/domain/useCases/userUseCases';
 import {
   IAuthenticateData,
   IFindAllUsersData,
+  IUpdatePasswordByEmailData,
+  IUpdateUserPasswordData,
   IUpdateUserRoleData,
   IUserCreateData,
 } from '@src/domain/util/models/userModels';
@@ -13,10 +15,11 @@ export interface IUserControllers {
   createAdmin(httpContext: IHttpContext): Promise<void>;
   createPlayer(httpContext: IHttpContext): Promise<void>;
   updateRole(httpContext: IHttpContext): Promise<void>;
-  // updatePassword(httpContext: IHttpContext): Promise<void>;
+  updatePassword(httpContext: IHttpContext): Promise<void>;
   findById(httpContext: IHttpContext): Promise<void>;
   findAll(httpContext: IHttpContext): Promise<void>;
   authenticate(httpContext: IHttpContext): Promise<void>;
+  sendTokenUpdatePasswordByEmail(httpContext: IHttpContext): Promise<void>;
 }
 
 export class UserControllers extends ErrorHandlerControllers implements IUserControllers {
@@ -70,19 +73,20 @@ export class UserControllers extends ErrorHandlerControllers implements IUserCon
     }
   }
 
-  // public async updatePassword(httpContext: IHttpContext): Promise<void> {
-  //   try {
-  //     const body = httpContext.getRequest().body as IUpdateUserPasswordData;
-  //     const updateUserPasswordData = {
-  //       id: body.id ?? '',
-  //       password: body.password ?? '',
-  //     };
-  //     const result = await this.userUseCases.updatePassword(updateUserPasswordData);
-  //     httpContext.send({ statusCode: 200, body: result });
-  //   } catch (error) {
-  //     httpContext.send(this.handleClientErrors(error));
-  //   }
-  // }
+  public async updatePassword(httpContext: IHttpContext): Promise<void> {
+    try {
+      const body = httpContext.getRequest().body as IUpdateUserPasswordData;
+      const updateUserPasswordData = {
+        token: body.token ?? '',
+        password: body.password ?? '',
+        confirmPassword: body.confirmPassword ?? '',
+      };
+      const result = await this.userUseCases.updatePassword(updateUserPasswordData);
+      httpContext.send({ statusCode: 200, body: result });
+    } catch (error) {
+      httpContext.send(this.handleClientErrors(error));
+    }
+  }
 
   public async findById(httpContext: IHttpContext): Promise<void> {
     try {
@@ -107,6 +111,21 @@ export class UserControllers extends ErrorHandlerControllers implements IUserCon
       const result = await this.userUseCases.findAll(findAllUsersData);
 
       httpContext.send({ statusCode: 200, body: result });
+    } catch (error) {
+      httpContext.send(this.handleClientErrors(error));
+    }
+  }
+
+  public async sendTokenUpdatePasswordByEmail(httpContext: IHttpContext): Promise<void> {
+    try {
+      const query = httpContext.getRequest().query ?? null;
+      const updatePasswordData = {
+        email: query?.email ?? '',
+      } as IUpdatePasswordByEmailData;
+
+      await this.userUseCases.sendTokenUpdatePasswordByEmail(updatePasswordData);
+
+      httpContext.send({ statusCode: 204, body: {} });
     } catch (error) {
       httpContext.send(this.handleClientErrors(error));
     }
